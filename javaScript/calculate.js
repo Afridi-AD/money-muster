b2c9ea7b3568c86146ef3a6fa2a0478df3966d2b
Afridi <thasinafridi8924@gmail.com>
2022-02-17 23:42:37 +0600

    remainaing balnce section update

--- javaScript/calculate.js
+++ javaScript/calculate.js
@@ -1,88 +1,98 @@
 function calculateMoney(){
     document.getElementById('calculate-button').addEventListener('click',function(){
-       
+    
+    
+        //    get input from income input 
+
+
        const incomeInput = document.getElementById('income-input');
        const incomeTotalText = incomeInput.value;
        const incomeTotal = parseInt(incomeTotalText);
+
+       //    get input from food input 
        
         const foodInput = document.getElementById('food-input');
         const foodCostText = foodInput.value;
         const foodCost = parseInt(foodCostText);
 
+       //    get input from rent input 
 
         const rentInput= document.getElementById('rent-input');
         const rentCostText = rentInput.value;
         const rentCost = parseInt(rentCostText);
 
+        //    get input from clothes input 
 
         const clothInput = document.getElementById('cloth-input');
         const clothCostText = clothInput.value;
         const clothCost = parseInt(clothCostText);
+        
+        
+        // calcullate total expenses 
 
         const totalExpenseValue = document.getElementById('total-expenses');
         const totalExpenseText = totalExpenseValue.innerText;
         const totalExpenses = parseInt(totalExpenseText);
-
-
         const subTotalExpense =  foodCost + rentCost + clothCost;
         totalExpenseValue.innerText = subTotalExpense;
 
+        //  calculate balence 
 
         const subTotalBalence = document.getElementById('balance');
         const balanceTex = subTotalBalence.innerText;
         const subBalence = parseInt(balanceTex);
-
         const balance = incomeTotal-subTotalExpense;
-        
         subTotalBalence.innerText =balance;
 
        
         
-        
-
-        
-
     })
 }
 
- calculateMoney()
+       calculateMoney()
 
 
 
- function calculateRemainingBalence(){
+  function calculateRemainingBalence(){
 
 
- document.getElementById('save-button').addEventListener('click',function(){
+     document.getElementById('save-button').addEventListener('click',function(){
 
-     
+    //  get income input 
+
     const incomeInput = document.getElementById('income-input');
     const incomeTotalText = incomeInput.value;
     const incomeTotal = parseInt(incomeTotalText);
+     
+    // get balence value 
 
     const subTotalBalence = document.getElementById('balance');
     const balanceTex = subTotalBalence.innerText;
     const subBalence = parseInt(balanceTex);
     
-    
+    // set save value 
     const saveInput = document.getElementById('save');
-     const saveText = saveInput.value;
-     const savePecentage = parseInt(saveText);
+    const saveText = saveInput.value;
+    const savePecentage = parseInt(saveText);
      
      
 
 
-     const saviningValue = document.getElementById('saving-amount');
-     const saviningValueText =saviningValue.innerText;
-     const savingAmount = parseInt(saviningValueText);
+    const saviningValue = document.getElementById('saving-amount');
+    const saviningValueText =saviningValue.innerText;
+    const savingAmount = parseInt(saviningValueText);
 
-     const saving = incomeTotal*savePecentage/100;
-     saviningValue.innerText= saving
+    const saving = incomeTotal*savePecentage/100;
+    saviningValue.innerText= saving
 
+    //  set remaining balance 
+    const remainingValue = document.getElementById('remaining-balance');
+    const remainingBalanceText = remainingValue.innerText;
+    const remainingBalenceValue = parseInt(remainingBalanceText);
+    const remainingBalence =  balance.innerText-saving;
+    remainingValue.innerText = remainingBalence;
 
-     const remainingValue = document.getElementById('remaining-balance');
-     const remainingBalanceText = remainingValue.innerText;
-     const remainingBalenceValue = parseInt(remainingBalanceText);
-     const remainingBalence = subBalence-saving;
+     
       
      calculateMoney();
 
